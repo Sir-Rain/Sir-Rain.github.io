4ce3afde1ca3aa96fdfefb1f660645b63f1c74b2
wk <liciaan@kakao.com>
2022-10-24 16:26:05 +0900

    add postitem

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,8 @@
 <script>
   import Header from '~/components/Header.svelte';
+  import PostItem from './components/PostItem.svelte';
 </script>
 
 <Header />
+
+<PostItem />
---
+++ src/components/PostItem.svelte
@@ -0,0 +1,21 @@
+<div class="post-item">
+  <div class="tags">
+    <ul class="flex">
+      <li class="mr-5"><a href="#">#tags</a></li>
+      <li class="mr-5"><a href="#">#tags2</a></li>
+      <li class="mr-5"><a href="#">#tags3</a></li>
+      <li class="mr-5"><a href="#">#tags4</a></li>
+    </ul>
+  </div>
+  <div class="title text-2xl font-bold">hello world !!!</div>
+  <div class="content">
+    유상민님 안녕하세요 :) 질문하신 내용은 webpack 명령을 실행할 수 없는
+    환경에서 발생합니다. 질문하신 내용과 관련한 답변을 최근에 GitHub에
+    업로드했습니다. 다음 링크('Webpack 사용 시 자주 묻는 질문') 참고하시고, 혹시
+    문제가 해결되지 않으시면 thesecon@gmail.com 으로 바로 연락주세요!
+  </div>
+  <div class="timestamp">2022-10-24</div>
+</div>
+
+<style>
+</style>
