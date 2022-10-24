272486ae326ee9dfdb324a99a71d71062703268f
wk <liciaan@kakao.com>
2022-10-24 13:36:39 +0900

    init prettier, tailwind css

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, '/src'),
+    },
+  },
   plugins: [svelte()],
   server: {
-    port: 3000
-  }
-})
+    port: 3000,
+  },
+});
