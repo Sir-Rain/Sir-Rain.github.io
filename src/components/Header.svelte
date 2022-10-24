fe31ef2af82e944dae44b7363bc0c031eb8ba20a
wk <liciaan@kakao.com>
2022-10-24 14:12:29 +0900

    init header

---
+++ src/components/Header.svelte
@@ -0,0 +1,10 @@
+<div class="flex justify-between">
+  <a href="">
+    <img src="" alt="" />
+    <span>prin.log</span>
+  </a>
+  <div class="navbar">
+    <a href="#">blog</a>
+    <a href="#">about</a>
+  </div>
+</div>
