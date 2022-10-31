df05ce12ada908643f8fdf282f6e48df95fc6359
wk <liciaan@kakao.com>
2022-10-31 14:17:06 +0900

    add footer

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,9 @@
 <script>
   import Header from '~/components/Header.svelte';
   import Layout from './layout/Layout.svelte';
+  import Footer from './components/Footer.svelte';
 </script>
 
 <Header />
 <Layout />
+<Footer />
---
+++ src/components/Footer.svelte
@@ -0,0 +1,19 @@
+<footer class="py-3 text-center">
+  <div>
+    <a href="/">prin.log</a>
+  </div>
+
+  <div>
+    <a href={void 0}>@{new Date().getFullYear()} prin</a>
+  </div>
+</footer>
+
+<style lang="scss">
+  a {
+    color: rgba(silver, 0.3);
+    text-decoration: none;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+</style>
