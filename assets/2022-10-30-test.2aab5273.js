8c937fde63ffded800146d874beb956973c2219f
Sir-Rain <liciaan@kakao.com>
2023-01-06 15:46:10 +0900

    Updates

---
+++ assets/2022-10-30-test.2aab5273.js
@@ -0,0 +1,3 @@
+const e={title:"Awesome Title2",description:"Describe this awesome content",timestamp:"2022-10-30T00:00:00.000Z",tags:["great","awesome","rad"]},t=`<h1>This is awesome</h1>
+<p>\uD14C\uC2A4\uD2B8\uB97C \uC880 \uD574\uBCF4\uACA0\uC74D\uB2C8\uB2E4.</p>
+`;export{e as attributes,t as html};
