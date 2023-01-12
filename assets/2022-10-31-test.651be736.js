9b94b1f235ee6c565fd384fd8051ad94449ab150
Sir-Rain <liciaan@kakao.com>
2023-01-12 15:11:54 +0900

    Updates

--- assets/2022-10-31-test.651be736.js
+++
@@ -1,3 +0,0 @@
-const e={title:"Awesome Title",description:"Describe this awesome content",timestamp:"2022-10-31T00:00:00.000Z",tags:["great","awesome","rad"]},t=`<h1>This is awesome</h1>
-<p>Vite is an opinionated web dev build tool that serves your code via native ES Module imports during dev and bundles it with Rollup for production.</p>
-`;export{e as attributes,t as html};
